baba946fef89ee4bca4ac7b3777f502874a822bd
tay <ajtr57@gmail.com>
2018-10-07 04:33:19 -0500

    Segunda Subida

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
+
 insert into alumnos(id,nombre,apellido) values (1,'Anderson','Taipe Reyes');
 insert into alumnos(id,nombre,apellido) values (2,'Alexis','Arancibia Sanches');
+insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,factor,comentario,id_alumno) values (1,1,'pma','pregunta1','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','asf','comentario',1);
