c460e009516da7100b7dea331f0fc6c8d005a7db
tay <ajtr57@gmail.com>
2018-10-09 12:40:59 -0500

    Tercera Subida

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,7 +12,7 @@ insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6
 insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (6,1,'pma','pregunta2','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa1','3','comentario',1);
 
 insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (7,1,'pma','pregunta1','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa2','4','comentario',1);
-insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (8,1,'pma','pregunta2','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa3','4','comentario',2);
+insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (8,1,'pma','pregunta2','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa5','4','comentario',2);
 
 insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (9,1,'pma','pregunta1','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa3','5','comentario',1);
 insert into encuestas(id_encuesta,id_pma,nom_encuesta,pregunta,a1,a2,a3,a4,a5,a6,respsel,factor,comentario,id_alumno) values (10,1,'pma','pregunta2','alternativa1','alternativa2','alternativa3','alternativa4','alternativa5','alternativa6','aternativa1','5','comentario',1);
